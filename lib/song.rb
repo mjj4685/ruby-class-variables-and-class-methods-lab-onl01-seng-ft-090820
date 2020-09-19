afc823bf899b4fdcc01f65700b33c21bb03139e4
MJ Johnson <mjohnson4685@gmail.com>
2020-09-19 22:36:56 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -30,7 +30,7 @@ class Song
   end
 
   def self.genre_count
-   genre_count = Hash.new { |hash, key| hash[key] =  }
+   genre_count = Hash.new(0)
    @@genres.each {|genre| genre_count[genre] +=1}
    genre_count
   end
