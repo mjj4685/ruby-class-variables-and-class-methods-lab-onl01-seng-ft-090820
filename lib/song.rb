a94a4d34683e6cbc10be6e7716fc61a20522fe05
MJ Johnson <mjohnson4685@gmail.com>
2020-09-19 22:06:46 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,15 +1,16 @@
 
 class Song
 
-  attr_accessor :name, :artist, :genre
-  @@count = 0
+   attr_accessor :name, :artist, :genre
+   @@count = 0
 
 
   def initialize(name, artist, genre)
     @@count += 1
   end
 
-  def song_count.count
+  def self.count
+    @@count
   end
 
 
